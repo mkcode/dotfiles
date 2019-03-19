0b119463969e8d2364b2b3c5ff21aea225c0bb11
Chris Ewald <chrisewald@gmail.com>
2019-03-18 20:52:06 -0400

    Many spoons configured

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -7,15 +7,38 @@ hs.logger.defaultLogLevel="info"
 hyper = {"cmd","ctrl"}
 
 hs.loadSpoon("SpoonInstall")
-
 spoon.SpoonInstall.use_syncinstall = true
-
 Install=spoon.SpoonInstall
+
+
+Install:andUse("BingDaily")
+
+Install:andUse("Caffeine",
+               {
+                 hotkeys = {
+                   toggle = { hyper, "c" } },
+                 start = true
+               }
+)
+
+-- Disable window size transition animations
+hs.window.animationDuration = 0.0
+Install:andUse("MiroWindowsManager",
+               {
+                 hotkeys = {
+                   up = {hyper, "k"},
+                   right = {hyper, "l"},
+                   down = {hyper, "j"},
+                   left = {hyper, "h"},
+                   fullscreen = {hyper, "f"}
+                 }
+               }
+)
 
 Install:andUse("TextClipboardHistory",
                {
                  config = {
-                   show_in_menubar = false,
+                   show_in_menubar = false
                  },
                  hotkeys = {
                    toggle_clipboard = { hyper, "v" } },
@@ -25,8 +48,24 @@ Install:andUse("TextClipboardHistory",
 
 Install:andUse("ReloadConfiguration",
                {
+                 config = {
+                   watch_paths = {
+                     hs.fs.pathToAbsolute(hs.configdir.."/init.lua") }
+                    -- hs.configdir }
+                 },
                  hotkeys = {
                    reloadConfiguration = { hyper, "=" } },
+                 start = true
+               }
+)
+
+Install:andUse("FadeLogo",
+               {
+                 config = {
+                   fade_in_time = 0,
+                   run_time = 0,
+                   fade_out_time = 0.4
+                 },
                  start = true
                }
 )
